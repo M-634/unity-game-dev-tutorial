444d3a875ac60d52e4f1163f5674ede7da47c00f
M-634 <miyanagateru634@gmail.com>
2025-05-06 21:59:49 +0900

    add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+namespace M634.Unity_Game_Dev_Tutorial
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        private enum MoveType
+        {
+            RigidbodyVelocity = 0,
+            Translate = 1
+        }
+        
+        [SerializeField]
+        private Rigidbody2D _rigidbody2D;
+        
+        [SerializeField]
+        private MoveType _moveType;
+        
+        [SerializeField]
+        private float _moveSpeed;
+        
+        private void Start()
+        {
+            if (_rigidbody2D == null)
+            {
+                _rigidbody2D = GetComponent<Rigidbody2D>();
+            }
+        }
+
+        private void Update()
+        {
+            if(_moveType != MoveType.Translate) return;
+            
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            
+            Vector2 velocity = new Vector2(h, v).normalized;
+            
+            transform.Translate(velocity * (_moveSpeed * Time.deltaTime));
+        }
+
+        private void FixedUpdate()
+        {
+            if(_moveType != MoveType.RigidbodyVelocity) return;
+            
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            
+            Vector2 velocity = new Vector2(h, v).normalized;
+         
+            _rigidbody2D.velocity = velocity * _moveSpeed;
+        }
+    }
+}
+
