004943f885282248b575aca153cd0b72a5ce0e6f
M-634 <miyanagateru634@gmail.com>
2025-05-17 09:31:35 +0900

    add enemy health control

--- Assets/Application/Scripts/Bullet.cs
+++ Assets/Application/Scripts/Bullet.cs
@@ -5,11 +5,28 @@ namespace Unity_Game_Dev_Tutorial
     [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
     public class Bullet : MonoBehaviour
     {
+        [SerializeField] 
+        private int _damageAmount = 10;
+        
+        [SerializeField] 
+        private bool _isDestroyedOnCollisionEnemy = false;
+        
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Wall"))
             {
-                 Destroy(gameObject);     
+                 Destroy(gameObject);  
+                 return;
+            }
+
+            if (collision.TryGetComponent<Enemy.EnemyHealth>(out var enemy))
+            {
+                 enemy.OnDamaged(_damageAmount);
+
+                 if (_isDestroyedOnCollisionEnemy)
+                 {
+                     Destroy(gameObject);
+                 }
             }
         }
     }
---
+++ Assets/Application/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Unity_Game_Dev_Tutorial.Enemy
+{
+    [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+    public class EnemyHealth : MonoBehaviour
+    {
+        [SerializeField] 
+        private int _health = 100;
+
+        [SerializeField]
+        private bool _isDead = false;
+        
+        public void OnDamaged(int damageAmount)
+        {
+            if(_isDead) return;
+            
+            _health -= damageAmount;
+
+            if (_health <= 0)
+            {
+                _isDead = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
