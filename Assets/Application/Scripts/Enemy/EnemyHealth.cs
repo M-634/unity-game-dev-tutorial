b24db5f6039e5fb0292c1cb3532a19869ad42b1b
M-634 <miyanagateru634@gmail.com>
2025-06-14 10:25:45 +0900

    add flash when enemy damaging

--- Assets/Application/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Application/Scripts/Enemy/EnemyHealth.cs
@@ -1,4 +1,5 @@
-﻿using Unity_Game_Dev_Tutorial.UI;
+﻿using System.Collections;
+using Unity_Game_Dev_Tutorial.UI;
 using UnityEngine;
 
 namespace Unity_Game_Dev_Tutorial.Enemy
@@ -8,6 +9,9 @@ namespace Unity_Game_Dev_Tutorial.Enemy
     {
         [SerializeField] 
         private int _maxHp = 100;
+        
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
         
         private int _currentHp;
         private bool _isDead = false;
@@ -23,14 +27,22 @@ namespace Unity_Game_Dev_Tutorial.Enemy
             
             DamagePopup.Create(transform.position, damageAmount);   
             _currentHp -= damageAmount;
+            StartCoroutine(Flash());
             
-
             if (_currentHp <= 0)
             {
                 _isDead = true;
                 EnemyKillCounter.Instance.AddKillCount();
                 Destroy(gameObject);
             }
+        }
+
+        private IEnumerator Flash()
+        {
+            var tempColor = _spriteRenderer.color;
+            _spriteRenderer.color = Color.red;
+            yield return new WaitForSeconds(0.2f);
+            _spriteRenderer.color = tempColor;
         }
     }
 }
