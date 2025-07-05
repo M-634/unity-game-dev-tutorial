99b61feb8379ccaf2648804b750f9ca6e20f4cbe
M-634 <miyanagateru634@gmail.com>
2025-07-05 10:54:38 +0900

    fix ExpPickupItem following player move

--- Assets/Application/Scripts/ExpPickup.cs
+++ Assets/Application/Scripts/ExpPickup.cs
@@ -10,13 +10,14 @@ namespace Unity_Game_Dev_Tutorial
         private int _expValue = 5;
 
         [SerializeField, Range(0f, 50f)]
-        private float _distanceToMove = 3f;
-
+        private float _distanceToMove = 10f;
+        
         [SerializeField, Range(0f, 5f)]
-        private float _moveDuration = 0.5f;
+        private float _moveTime = 0.3f;
 
         private Transform _player;
-        private bool _isMoving = false;
+        private bool _isMoving;
+        private Tweener _moveTween;
 
         private void Start()
         {
@@ -26,19 +27,40 @@ namespace Unity_Game_Dev_Tutorial
         private void Update()
         {
             if (_isMoving || _player == null) return;
-
+            
             float distance = Vector2.Distance(transform.position, _player.position);
             if (distance < _distanceToMove)
             {
                 _isMoving = true;
-                transform.DOMove(_player.position, _moveDuration).SetEase(Ease.InOutSine);
+                StartFollowingPlayer();
+                Debug.Log($"{gameObject.name} start moving");
             }
+        }
+
+        private void StartFollowingPlayer()
+        {
+            _moveTween = transform
+                .DOMove(_player.position, _moveTime)
+                .SetEase(Ease.Linear)
+                .SetLoops(-1, LoopType.Restart)
+                .OnUpdate(() =>
+                {
+                    if (_moveTween.IsActive())
+                    {
+                        _moveTween.ChangeEndValue(_player.position, true);
+                    }
+                });
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player"))
             {
+                if (_moveTween != null && _moveTween.IsActive())
+                {
+                    _moveTween.Kill(); 
+                }
+                
                 if (PlayerLevelManager.Instance != null)
                 {
                     PlayerLevelManager.Instance.AddExp(_expValue);
