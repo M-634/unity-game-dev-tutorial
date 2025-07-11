c9ca59b45d001205bf135e1ed3cc0b1a77075dc0
M-634 <miyanagateru634@gmail.com>
2025-07-12 00:28:55 +0900

    add title and result script

---
+++ Assets/Application/Scripts/Result.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Unity_Game_Dev_Tutorial
+{
+    public class Result : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI _killText;
+
+        [SerializeField]
+        private TextMeshProUGUI _timeText;
+
+        private void Start()
+        {
+            int kills = GameManager.Instance?.KillCount ?? 0;
+            float time = GameManager.Instance?.ElapsedTime ?? 0f;
+
+            _killText.text = $"撃破数: {kills}";
+            _timeText.text = $"生存時間: {Mathf.FloorToInt(time / 60):00}:{Mathf.FloorToInt(time % 60):00}";
+        }
+
+        public void OnRetryButton()
+        {
+            SceneLoader.LoadGame();
+        }
+
+        public void OnTitleButton()
+        {
+            SceneLoader.LoadTitle();
+        }
+    }
+}
---
+++ Assets/Application/Scripts/Title.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unity_Game_Dev_Tutorial
+{
+    public class Title : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _loadGameButton;
+        
+        [SerializeField]
+        private Button _quitGameButton;
+
+        private void Start()
+        {
+            _loadGameButton.onClick.AddListener(OnStartButton);
+            _quitGameButton.onClick.AddListener(OnQuitButton);
+        }
+
+        private void OnDestroy()
+        {
+            _loadGameButton.onClick.RemoveListener(OnStartButton);
+            _quitGameButton.onClick.RemoveListener(OnQuitButton);
+        }
+
+        private void OnStartButton()
+        {
+            SceneLoader.LoadGame();
+        }
+
+        private void OnQuitButton()
+        {
+            SceneLoader.QuitGame();
+        }
+    }
+}
