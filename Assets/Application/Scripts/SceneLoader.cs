34ad3a503285bbad44165943de7379987cb75c04
M-634 <miyanagateru634@gmail.com>
2025-07-11 23:56:28 +0900

    add SceneLoader.cs

---
+++ Assets/Application/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unity_Game_Dev_Tutorial
+{
+    public static class SceneLoader 
+    {
+        public static void LoadGame()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        public static void LoadTitle()
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+
+        public static void LoadResult()
+        {
+            SceneManager.LoadScene("ResultScene");
+        }
+
+        public static void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
