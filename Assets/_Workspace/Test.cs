91125057af8d643efc7d01934238f53fc1ad43fb
M-634 <miyanagateru634@gmail.com>
2026-02-23 20:24:43 +0900

    update unity 6000.3.9f1

--- Assets/_Workspace/Test.cs
+++ Assets/_Workspace/Test.cs
@@ -10,7 +10,11 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+#if APP_DEBUG
+        Debug.Log("Scripting Define Symbolsに'APP_DEBUG'を追加した際にコンパイルされる");
+#else
+        Debug.Log("Scripting Define Symbolsに'APP_DEBUG'が存在しない際にコンパイルされる");
+#endif
     }
 
     // Update is called once per frame
